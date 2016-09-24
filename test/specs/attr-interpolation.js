a4d0bc2ff5ff4c161a949c9f464d22024820f015
Ryan Morr <rm.morr@gmail.com>
2016-09-24 13:35:16 -0400

    Update suite name

--- test/specs/attr-interpolation.js
+++ test/specs/attr-interpolation.js
@@ -4,7 +4,7 @@ import { expect } from 'chai';
 import sinon from 'sinon';
 import templar from '../../src/templar';
 
-describe('attr interpolation', () => {
+describe('attribute interpolation', () => {
     it('should support interpolation', () => {
         const tpl = templar('<div id="{{id}}"></div>');
         tpl.set('id', 'foo');
